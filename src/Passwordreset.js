f8cd25bc98e10ec8ca3048d0b7c026c05fc135f6
Mugesh <mugeshkumark22@gmail.com>
2022-11-02 15:31:25 +0530

    final

--- src/Passwordreset.js
+++ src/Passwordreset.js
@@ -1,52 +1,46 @@
 import React, { useState } from "react";
-// import { env } from "../config";
 import { useFormik } from "formik";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Button, TextField, Typography } from "@mui/material";
 
 function Passwordreset() {
-  // let formik = useFormik({
-  //   initialValues: {
-  //     email: "",
-  //   },
-  //   onSubmit: async (values) => {
-  //     let user = await axios.post('http://localhost:3001/forgotpassword', values);
-  //     console.log(user);
-  //     alert(user.data.message);
-  //   },
-  // });
+  
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
+ 
+  let formik = useFormik({
+    initialValues: {
       email: "",
-    
-  })
-
-  const handleSubmit = async (e) => {
-      e.preventDefault();
-      const response = await axios.post("https://passresetbackend22.herokuapp.com/forgotpassword", { ...formData });
-     console.log(response);
-     if(response.status===200){
-      alert("Email Sent")
-      navigate("/")
-     }else{
-      alert("Enter Valid email or Existing email")
-     }
-  }
+    },
+    validate: (values) => {
+        let errors = {};
+        if (values.email === "") {
+            errors.email = "Please Enter Email"
+        }
+        return errors;
+    },
+    onSubmit: async (values) => {
+      let user = await axios.post('https://passresetbackend22.herokuapp.com/forgotpassword', values);
+      console.log(user);
+    }
+   
+  });
+ 
   return (
     <div style={{ margin: "10%", paddingLeft: "30%" }}><Typography variant="h4">
     Reset Password
 </Typography>
     <br />
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
         <div>
             <TextField id="filled-basic" label="Email"
                 name="email"
                 variant="filled"
                 type="email" style={{ width: "35%" }} placeholder="Enter Your Email"
-                value={formData.email}
-                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                value={formik.values.email} onChange={formik.handleChange}
             />
+            <br/>
+            <span style={{ color: 'red' }}>{formik.errors.email}</span>
         </div>
         <br />
         <Button variant="contained" type="submit">Reset Password</Button>
